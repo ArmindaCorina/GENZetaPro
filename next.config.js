b3c7e8d563e83f0a4b0a2d6d67b13ab3e8090ad6
ArmindaCorina <potenciatuvalor@gmail.com>
2025-08-04 16:40:10 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
 export default function Home() {
   return <h1>¡Hola, Next.js en GitHub Pages!</h1>;
 }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/GENZetaPro',
+  assetPrefix: '/GENZetaPro'
+}
+module.exports = nextConfig
